6b10b63b52967a88e9b87bfd29944e29bf3d23bc
mrodlom1177 <mrodlom11577@gmail.com>
2026-05-27 11:01:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from openai import OpenAI
 import streamlit as st
+from openai import OpenAI
 
-client = OpenAI(
-    api_key=st.secrets["OPENAI_API_KEY"]
-)
+st.write("APP INICIANDO...")
+
+try:
+
+    client = OpenAI(
+        api_key=st.secrets["OPENAI_API_KEY"]
+    )
+
+    st.success("OPENAI CONECTADO")
+
+except Exception as e:
+
+    st.error(e)
+
+    st.stop()
